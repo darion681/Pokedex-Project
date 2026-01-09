994ac1e56500e737bb41ca68f9b463e91f16ae40
Darion <darion.duncan@bcestudent.com>
2026-01-09 12:14:24 +0000

    Implement basic GUI layout for Pokedex
    Added GUI components for Pokedex using Dear PyGui.

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib as plt
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 from importlib.metadata import version as pkg_version
 from packaging import version
 import sys
@@ -60,3 +61,39 @@ if outdated:
     sys.exit()
 
 print("All dependencies are up to date. Launching Pokedex...")
+
+# GUI - Possibly add to seperate filelater
+
+dpg.create_context()
+
+# with dpg.window(tag="Primary Window"):
+#     dpg.add_text("Hello, world")
+#     dpg.add_button(label="Save")
+#     dpg.add_input_text(label="string", default_value="Quick brown fox")
+#     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+# dpg.create_viewport(title='Custom Title', width=600, height=200)
+# dpg.setup_dearpygui()
+# dpg.show_viewport()
+# dpg.set_primary_window("Primary Window", True)
+# dpg.start_dearpygui()
+# dpg.destroy_context()
+
+dpg.create_context()
+
+with dpg.window(label="Pokedex"):
+    dpg.add_button(label="Button 1")
+    dpg.add_button(label="Button 2")
+    with dpg.group():
+        dpg.add_button(label="Button 3")
+        dpg.add_button(label="Button 4")
+        with dpg.group() as group1:
+            pass
+dpg.add_button(label="Button 6", parent=group1)
+dpg.add_button(label="Button 5", parent=group1)
+
+dpg.create_viewport(title='Pokedex.exe', width=600, height=400)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
